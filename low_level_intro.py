d050cc6510781e261a3f51aac2208fbc0ca3df41
mp3242 <mp3242@columbia.edu>
2018-02-28 09:14:18 -0500

    remove accuracies and update notebook

--- low_level_intro.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-x = tf.placeholder(tf.float32, shape=[None, 3])
-linear_model = tf.layers.Dense(units=1)
-y = linear_model(x)
-
-writer = tf.summary.FileWriter('.')
-writer.add_graph(tf.get_default_graph())
-writer.flush()
